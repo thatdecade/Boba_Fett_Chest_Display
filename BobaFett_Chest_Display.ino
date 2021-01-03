720bc57868aca1848cb907d38890599a38e6fdf4
Dustin Westaby <thatdecade@gmail.com>
2021-01-03 13:40:24 -0600

    Mandalorian sequence added.
    Reworked animation routines to allow the bargraph to be run async from the character animation.

--- BobaFett_Chest_Display.ino
+++ BobaFett_Chest_Display.ino
@@ -3,7 +3,7 @@
   This is the software for the display on Boba Fett’s chest plate.
   Arduino IDE.
 
-  Last Modified: Jan 2, 2016
+  Last Modified: Dec 18, 2020
 
   Authors: Joshua Kane, Dustin Westaby
 
@@ -19,77 +19,168 @@
   PCB Bargraph Board:
   https://www.oshpark.com/shared_projects/E4qjCF4r
 
-  Usage:
-  Some example sequences are given below. For a customized sequence:
-   - Download the Board Sequence Builder from fettronics.com
-   - Edit the sequence builder's .ini file with the following:
-      segmentorder=-gfedcba
-      toporder=---12345
-   - Open the Board Sequence Builder application and create your new sequence.
-   - When finished, save, close, and reopen the .ini file.  Find your sequence on the list.
-   - Copy and paste the hex numbers below in the appropriate locations
-   - Update SEQUENCE_SIZE with the number of lines in your animation sequence
-
+  Usage: 
+   - Some example sequences are given below. Comment / Uncomment the sequence you want to use.
+   - For a customized sequence, use the sequence_builder.xlsx
+   - To synch the bargraph and character animations, use the same divider and the same number of sequence steps.
 */
 
  /* =========================
       Structure Constants
    ========================= */
 
-#define SEQUENCE_SIZE 11
+/* START: Mandalorian Animation */
+
+#define BAR_SEQ_SIZE 8
+#define CHAR_SEQUENCE_SIZE 10
+
+//higher = slower
+#define CHAR_ANIMATION_SPEED_DIVIDER 9  // Sets divider, NUM_LOOPS_PER_WORD / 2^X
+#define BAR_ANIMATION_SPEED_DIVIDER  7  // Sets divider, NUM_LOOPS_PER_WORD / 2^X
+
+const byte char_sequence[10][5] =
+{
+    //  DIGIT 1     DIGIT 2     DIGIT 3     DIGIT 4     DIGIT 5  
+    //  GFEDCBA     GFEDCBA     GFEDCBA     GFEDCBA     GFEDCBA         
+    {0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111},            // 0
+    {0b10001001, 0b11000010, 0b10100100, 0b10000000, 0b11001110}, //HG28|    // 1
+    {0b10000000, 0b10000000, 0b10000000, 0b10000000, 0b10000000}, //88888    // 2
+    {0b10111101, 0b11011011, 0b11110110, 0b11001111, 0b10111110}, // -'',_|- // 3
+    {0b10100001, 0b11000001, 0b10000110, 0b10001100, 0b11111101},  //dUEP'   // 4
+    {0b10001001, 0b10010011, 0b10100100, 0b10011000, 0b11001111}, //H5291    // 5
+    {0b10001010, 0b10000110, 0b10101000, 0b11100000, 0b10001111}, //hEaJt    // 6
+    {0b10000110, 0b10000110, 0b10101100, 0b11110000, 0b10011111}, //EE2Cl    // 7
+    {0b10111001, 0b11010111, 0b11111110, 0b11001111, 0b10111110}, //t,-|_    // 8
+    {0b10011010, 0b10000110, 0b10111101, 0b11100000, 0b10011111}, //5E-J-    // 9
+};
+
+const byte bar_sequence[8] =
+{           //  BARGRAPH
+            //   12345
+    0x00, //  0 |    =|
+    0x01, //  1 |   = |
+    0x02, //  2 |  =  |
+    0x03, //  3 | =   |
+    0x04, //  4 |=    |
+    0x03, //  5 | =   |
+    0x02, //  6 |  =  |
+    0x01, //  7 |   = |
+};
+/* // END: Mandalorian Animation */
+
+/* START: ESB Animation 
+ 
+//higher = slower (max = 10)
+#define CHAR_ANIMATION_SPEED_DIVIDER  10
+#define BAR_ANIMATION_SPEED_DIVIDER   10
+
+const byte char_sequence[11][5] =
+{
+    //  DIGIT 1     DIGIT 2     DIGIT 3     DIGIT 4     DIGIT 5  
+    //  GFEDCBA     GFEDCBA     GFEDCBA     GFEDCBA     GFEDCBA         
+    {0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111},  //      // 0
+    {0b10100001, 0b11000001, 0b10000110, 0b10001100, 0b11111101},  //dUEP' // 1
+    {0b10000000, 0b10000000, 0b10000000, 0b10000000, 0b10000000},  //88888 // 2
+    {0b10100001, 0b11000001, 0b10000110, 0b10001100, 0b11111101},  //dUEP' // 3
+    {0b10001011, 0b10000110, 0b10001000, 0b10100001, 0b10001001},  //hEADH // 4
+    {0b10111101, 0b11011011, 0b11110110, 0b11001111, 0b10111110},  //'.=¦= // 5
+    {0b10100001, 0b11000001, 0b10000110, 0b10001100, 0b11111101},  //dUEP' // 6
+    {0b10001001, 0b10010010, 0b10100100, 0b10011000, 0b11111001},  //H5291 // 7
+    {0b10111101, 0b11011011, 0b11110110, 0b11001111, 0b10111110},  //'.=¦= // 8
+    {0b10100001, 0b11000001, 0b10000110, 0b10001100, 0b11111101},  //dUEP' // 9
+    {0b10001001, 0b11000010, 0b10100100, 0b10000000, 0b11001110},  //H{E8¬ // 10
+};
+
+const byte bar_sequence[11] =
+{           //  BARGRAPH
+            //   12345
+    0xFF, //  0 |     |
+    0x01, //  1 | =   |
+    0x03, //  2 |   = |
+    0x02, //  3 |  =  |
+    0x04, //  4 |    =|
+    0xFF, //  5 |     |
+    0x01, //  6 | =   |
+    0x00, //  7 |=    |
+    0xFF, //  8 |     |
+    0x01, //  9 | =   |
+    0xFF, // 10 |     |
+};
+// END: ESB Animation */
+
+
+/* START: ROTJ Animation 
+ 
+//higher = slower
+#define CHAR_ANIMATION_SPEED_DIVIDER  10
+#define BAR_ANIMATION_SPEED_DIVIDER   10
+
+const byte char_sequence[8][5] =
+{
+    //  DIGIT 1     DIGIT 2     DIGIT 3     DIGIT 4     DIGIT 5  
+    //  GFEDCBA     GFEDCBA     GFEDCBA     GFEDCBA     GFEDCBA         
+    {0b10000000, 0b10000000, 0b10000000, 0b10000000, 0b10000000},  //88888 // 0
+    {0b10111101, 0b11011011, 0b11110110, 0b11001111, 0b10111110},  //'.=¦= // 1
+    {0b10100001, 0b11000001, 0b10000110, 0b10001100, 0b11111101},  //dUEP' // 2
+    {0b10001001, 0b11000010, 0b10100100, 0b10000000, 0b11001110},  //HG28r // 3
+    {0b10001001, 0b10010010, 0b10100100, 0b10011000, 0b11111001},  //H5291 // 4
+    {0b10111101, 0b11011011, 0b11110110, 0b11001111, 0b10111110},  //'.=¦= // 5
+    {0b10001011, 0b10000110, 0b10001000, 0b10100001, 0b10001001},  //hEADH // 6
+    {0b10001001, 0b11000010, 0b10100100, 0b10000000, 0b11001110},  //HG28r // 7
+};
+
+const byte bar_sequence[8] =
+{           //  BARGRAPH
+            //   12345
+    0x03, //  0 |   = |
+    0xFF, //  1 |     |
+    0x02, //  2 |  =  |
+    0xFF, //  3 |     |
+    0x00, //  4 |=    |
+    0xFF, //  5 |     |
+    0x04, //  6 |    =|
+    0xFF, //  7 |     |
+};
+// END: ROTJ Animation */
+
+
+/* *************************************************** */
+/* DO NOT EDIT BELOW THIS LINE */
+/* *************************************************** */
+
+// This section contains speed calculations
+
+#define ANIMATION_SPEED_MULTIPLIER 100  // Sets multiplier, CHAR_SEQUENCE_SIZE * BAR_SEQ_SIZE * X
+
+//NUM_LOOPS_PER_WORD needs to be divisible by bar_sequence and char_sequence's size
+#define NUM_LOOPS_PER_WORD (CHAR_SEQUENCE_SIZE*BAR_SEQ_SIZE*CHAR_ANIMATION_SPEED_DIVIDER*ANIMATION_SPEED_MULTIPLIER) 
+
+/* ********** Example speed calc **********
+     speed multipler = 100
+     char speed divider = 9   //2^9 = 512
+     bar speed divider  = 6   //2^6 = 64
+     
+     NUM_LOOPS_PER_WORD = (CHAR_SEQUENCE_SIZE*BAR_SEQ_SIZE*ANIMATION_SPEED_MULTIPLIER*CHAR_ANIMATION_SPEED_DIVIDER*BAR_ANIMATION_SPEED_DIVIDER) = (10*8*9*100) = 72000
+     
+     for ( NUM_LOOPS_PER_WORD )
+        charIdx = (i >> CHAR_ANIMATION_SPEED_DIVIDER) % CHAR_SEQ_SIZE = (i / 64) % 10
+        barIdx  = (i >> BAR_ANIMATION_SPEED_DIVIDER)  % BAR_SEQ_SIZE  = (i / 64) % 8
+
+     // Every 512th, the character will update, sending an index of 0-9 to word_test
+     // Every 64th loop, the bar will update, sending an index of 0-7 to bar_test
+     
+********** ********** ********** ********** */
+
+// This section contains board specific information
+
 #define NUM_BARS       5
 #define NUM_DIGITS     5
 #define NUM_SEGMENTS   7
 
-// Animation Speed
-#define NUM_LOOPS_PER_WORD 300*8
-
-/*
-  For ESB:
-*/
-
-const byte char_sequence[SEQUENCE_SIZE][NUM_DIGITS + 1] =
-{
-  //  DIGIT 1     DIGIT 2     DIGIT 3     DIGIT 4     DIGIT 5  BAR (0-4)        DISPLAY  BARGRAPH
-  //  GFEDCBA     GFEDCBA     GFEDCBA     GFEDCBA     GFEDCBA                     12345   01234
-  {0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0xFF},  //Seq 1  -        |     |
-  {0b10100001, 0b11000001, 0b10000110, 0b10001100, 0b11111101, 0x01},  //Seq 2  - dUEP'  | =   |
-  {0b10000000, 0b10000000, 0b10000000, 0b10000000, 0b10000000, 0x03},  //Seq 3  - 88888  |   = |
-  {0b10100001, 0b11000001, 0b10000110, 0b10001100, 0b11111101, 0x02},  //Seq 4  - dUEP'  |  =  |
-  {0b10001011, 0b10000110, 0b10001000, 0b10100001, 0b10001001, 0x04},  //Seq 5  - hEADH  |    =|
-  {0b10111101, 0b11011011, 0b11110110, 0b11001111, 0b10111110, 0xFF},  //Seq 6  - '.=¦=  |     |
-  {0b10100001, 0b11000001, 0b10000110, 0b10001100, 0b11111101, 0x01},  //Seq 7  - dUEP'  | =   |
-  {0b10001001, 0b10010010, 0b10100100, 0b10011000, 0b11111001, 0x00},  //Seq 8  - H5291  |=    |
-  {0b10111101, 0b11011011, 0b11110110, 0b11001111, 0b10111110, 0xFF},  //Seq 9  - '.=¦=  |     |
-  {0b10100001, 0b11000001, 0b10000110, 0b10001100, 0b11111101, 0x01},  //Seq 10 - dUEP'  | =   |
-  {0b10001001, 0b11000010, 0b10100100, 0b10000000, 0b11001110, 0xFF},  //Seq 11 - H{E8¬  |     |
-};
-
-/* For ROTJ:
-   - Change SEQUENCE_SIZE to 8
-   - Copy and paste the following into char_sequence below:
-
-const byte char_sequence[SEQUENCE_SIZE][NUM_DIGITS + 1] =
-{
-  //  DIGIT 1     DIGIT 2     DIGIT 3     DIGIT 4     DIGIT 5  BAR (0-4)        DISPLAY  BARGRAPH
-  //  GFEDCBA     GFEDCBA     GFEDCBA     GFEDCBA     GFEDCBA                     12345   01234
-  {0b10000000, 0b10000000, 0b10000000, 0b10000000, 0b10000000, 0x03},  //Seq 1  - 88888  |   = |
-  {0b10111101, 0b11011011, 0b11110110, 0b11001111, 0b10111110, 0xFF},  //Seq 2  - '.=¦=  |     |
-  {0b10100001, 0b11000001, 0b10000110, 0b10001100, 0b11111101, 0x02},  //Seq 3  - dUEP'  |  =  |
-  {0b10001001, 0b11000010, 0b10100100, 0b10000000, 0b11001110, 0xFF},  //Seq 4  - HG28r  |     |
-  {0b10001001, 0b10010010, 0b10100100, 0b10011000, 0b11111001, 0x00},  //Seq 5  - H5291  |=    |
-  {0b10111101, 0b11011011, 0b11110110, 0b11001111, 0b10111110, 0xFF},  //Seq 6  - '.=¦=  |     |
-  {0b10001011, 0b10000110, 0b10001000, 0b10100001, 0b10001001, 0x04},  //Seq 7 -  hEADH  |    =|
-  {0b10001001, 0b11000010, 0b10100100, 0b10000000, 0b11001110, 0xFF},  //Seq 8  - HG28r  |     |
-};
-*/
-
-#define BAR_DATA    NUM_DIGITS
 #define BAR_ON      LOW
 #define BAR_OFF     HIGH
 
-// Setup PinMap for bars at the top of the kit
-const byte pinmap_bars[5] =
+const byte pinmap_bars[NUM_BARS] =
 {
 	11,   // Bar 1
 	12,   // Bar 2
@@ -98,8 +189,7 @@ const byte pinmap_bars[5] =
 	3,    // Bar 5
 };
 
-// Setup PinMap for the individual segements in each Character in the Kit
-const byte pinmap_segments[7] =
+const byte pinmap_segments[NUM_SEGMENTS] =
 {
 	0,   // Segment A
 	1,   // Segment B
@@ -110,8 +200,7 @@ const byte pinmap_segments[7] =
 	8,   // Segment G
 };
 
-// Setup PinMap that controls the selection of each Character in the Kit
-const byte pinmap_digits[5] =
+const byte pinmap_digits[NUM_DIGITS] =
 {
 	9,    // Digit 1
 	10,   // Digit 2
@@ -119,6 +208,10 @@ const byte pinmap_digits[5] =
 	15,   // Digit 4
 	16,   // Digit 5
 };
+
+/* *************************************************** */
+/* CODE */
+/* *************************************************** */
 
 // the setup routine runs once when you press reset:
 void setup()
@@ -145,25 +238,22 @@ void setup()
   }
 }
 
-// the loop routine runs over and over again forever:
-void loop()
-{
-  sentence_test();
-}
-
 // Walks through the entire sentence, word by word, repeating each word
 //   multiple times so that they mind perceives a roughly two second delay
 //   between each new word before the loop starts again.
-void sentence_test()
+void loop()
 {
-  // Move through the words in the sentence one at a time
-  for ( int idxWord = 0; idxWord < SEQUENCE_SIZE; idxWord++)
+  uint16_t charIdx = 0;
+  uint16_t barIdx = 0;
+
+  // Repeat each word to meet the desired update frequency (300 = ~2 seconds)
+  for ( uint32_t idxDelay = 0; idxDelay < NUM_LOOPS_PER_WORD; idxDelay++)
   {
-    // Repeat each word to meet the desired update frequency (300 = ~2 seconds)
-    for ( int idxDelay = 0; idxDelay < NUM_LOOPS_PER_WORD; idxDelay++)
-    {
-      word_test( idxWord ) ;
-    }
+    barIdx  = (idxDelay>>BAR_ANIMATION_SPEED_DIVIDER)  % BAR_SEQ_SIZE;
+    charIdx = (idxDelay>>CHAR_ANIMATION_SPEED_DIVIDER) % CHAR_SEQUENCE_SIZE;
+    
+    bar_test ( barIdx  );
+    word_test( charIdx ) ;
   }
 }
 
@@ -172,9 +262,6 @@ void sentence_test()
 //   to see the letter even though it's on only 1/5 of the time.
 void word_test(int idxWord )
 {
-  // Turn on the correct bar
-  digitalWrite( pinmap_bars[char_sequence[idxWord][BAR_DATA]], BAR_ON ) ;
-
   // Now, let's work on the word we would like to display
   for( int character = 0 ; character < NUM_DIGITS; character++ )
   {
@@ -193,8 +280,15 @@ void word_test(int idxWord )
     digitalWrite( pinmap_digits[character],   LOW);
   }
 
-  // Turn the correct bar off
-  digitalWrite( pinmap_bars[char_sequence[idxWord][BAR_DATA]], BAR_OFF );
 }
 
-
+void bar_test(int index)
+{
+  static int old_index = 0;
+  if(index != old_index)
+  {
+    digitalWrite( pinmap_bars[bar_sequence[old_index]], BAR_OFF);
+    digitalWrite( pinmap_bars[bar_sequence[index]],     BAR_ON);
+    old_index = index;
+  }
+}
